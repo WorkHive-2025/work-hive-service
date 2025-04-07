828f752856e0316e679a1d75cafe0f92a015f175
wo_okis <chanuk9531@daum.net>
2025-04-07 21:37:22 +0900

    feat: global config 추가

--- src/main/java/workhive/app/AppApplication.java
+++ src/main/java/workhive/app/AppApplication.java
@@ -2,7 +2,9 @@ package workhive.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class AppApplication {
 
