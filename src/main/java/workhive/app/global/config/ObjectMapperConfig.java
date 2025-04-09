c6054ae9705d9ee14b7136e0a716f37655095cdb
wo_okis <chanuk9531@daum.net>
2025-04-09 21:51:46 +0900

    feat: ojbetMapper Config 추가

---
+++ src/main/java/workhive/app/global/config/ObjectMapperConfig.java
@@ -0,0 +1,26 @@
+package workhive.app.global.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        mapper.registerModule(javaTimeModule);
+        configureMapper(mapper);
+        return mapper;
+    }
+
+    private void configureMapper(ObjectMapper mapper) {
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+    
+}
