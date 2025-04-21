b16e7cf3dc6e8f0c0b25eb26fff13dfbf7f659a9
wo_okis <chanuk9531@daum.net>
2025-04-21 23:30:07 +0900

    feat: 권한 확인 로직 추가

--- src/main/java/workhive/app/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/workhive/app/auth/service/UserDetailsServiceImpl.java
@@ -1,5 +1,6 @@
 package workhive.app.auth.service;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,9 +8,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.util.AntPathMatcher;
 import workhive.app.auth.entity.MemberRole;
 import workhive.app.auth.entity.Role;
 import workhive.app.auth.repository.MemberRoleRepository;
+import workhive.app.auth.vo.PermissionVo;
 import workhive.app.auth.vo.UserDetailsImpl;
 import workhive.app.member.entity.Member;
 import workhive.app.member.service.MemberService;
@@ -24,6 +27,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final MemberService memberService;
     private final MemberRoleRepository memberRoleRepository;
+    private final RolePermissionServiceImpl rolePermissionCacheService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -41,5 +45,72 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
         //4. UserDetailsImpl 객체 생성 및 반환
         return new UserDetailsImpl(member, roles);
+    }
+
+    public boolean checkRequestPermission(List<String> roles, HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        String method = request.getMethod();
+
+        if (requestURI == null || method == null) {
+            return false; // 요청 URI 또는 메서드가 null인 경우 권한 거부
+        }
+
+        //1. Redis에서 권한 검사
+        boolean redisAuthorized = checkAuthorizationByRedis(roles, requestURI, method);
+        if (redisAuthorized) {
+            return true; // Redis에서 권한 허용
+        }
+
+        //2. DB에서 권한 검사
+        boolean dbAuthorized = checkAuthorizationByDB(roles, requestURI, method);
+        if (dbAuthorized) {
+            return true; // DB에서 권한 허용
+        }
+
+        return false; // 권한 거부
+    }
+
+    public boolean checkAuthorizationByRedis(List<String> roles,
+                                             String requestURI,
+                                             String method) {
+
+        AntPathMatcher matcher = new AntPathMatcher();
+
+        //1. 요청 URI와 메서드에 대한 권한 검사
+        for (String role : roles) {
+            List<PermissionVo> permissions = rolePermissionCacheService.getPermissionsByRoleCodeInRedis(role);
+            for (PermissionVo permission : permissions) {
+                boolean pathMatch = matcher.match(permission.getRequestPath(), requestURI);
+                boolean methodMatch = "*".equals(permission.getHttpMethod()) || permission.getHttpMethod().equalsIgnoreCase(method);
+
+                if (pathMatch && methodMatch) {
+                    return true; // 권한 허용
+                }
+            }
+        }
+
+        return false; // 권한 거부
+    }
+
+    public boolean checkAuthorizationByDB(List<String> roles,
+                                           String requestURI,
+                                           String method) {
+
+        AntPathMatcher matcher = new AntPathMatcher();
+
+        //1. 요청 URI와 메서드에 대한 권한 검사
+        for (String role : roles) {
+            List<PermissionVo> permissions = rolePermissionCacheService.getPermissionsByRoleCodeInDB(role);
+            for (PermissionVo permission : permissions) {
+                boolean pathMatch = matcher.match(permission.getRequestPath(), requestURI);
+                boolean methodMatch = "*".equals(permission.getHttpMethod()) || permission.getHttpMethod().equalsIgnoreCase(method);
+
+                if (pathMatch && methodMatch) {
+                    return true; // 권한 허용
+                }
+            }
+        }
+
+        return false; // 권한 거부
     }
 }
--- src/main/java/workhive/app/global/manager/AuthorizationManagerImpl.java
+++ src/main/java/workhive/app/global/manager/AuthorizationManagerImpl.java
@@ -6,15 +6,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.stereotype.Component;
+import workhive.app.auth.service.UserDetailsServiceImpl;
+import workhive.app.auth.vo.UserDetailsImpl;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class AuthorizationManagerImpl implements AuthorizationManager<RequestAuthorizationContext> {
+
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext context) {
@@ -26,11 +32,14 @@ public class AuthorizationManagerImpl implements AuthorizationManager<RequestAut
             return new AuthorizationDecision(false);
         }
 
-        //TODO : 권한 검사 로직 추가
-        // 예시: 특정 URL에 대한 권한 검사
-        // boolean result = checkPermission(request, auth);
-        // return new AuthorizationDecision(result);
+        //권한 검사 로직 추가
+        UserDetailsImpl principal = (UserDetailsImpl) auth.getPrincipal();
+        List<String> authorities = principal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
 
-        return new AuthorizationDecision(true);
+        boolean authorized = userDetailsService.checkRequestPermission(authorities, request);
+        return new AuthorizationDecision(authorized);
     }
 }
